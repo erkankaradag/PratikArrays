3b583d8605edb29df5aead32e0ab8fb9aa2e5a96
erkankaradag <erkankaradagcom@gmail.com>
2025-01-17 12:07:52 +0300

    Add project files.

---
+++ PratikArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] dizi = new int[10];
+
+        // Diziyi elemanlarını gir
+        for (int i = 0; i < dizi.Length; i++)
+        {
+            Console.WriteLine($"Dizinin {i + 1}. elemanını girin!");
+            dizi[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Dizinin elemanlarını ekrana yazdır
+        Console.WriteLine("Dizinin elemanları: ");
+        foreach (int eleman in dizi)
+        {
+            Console.WriteLine($"Dizinin elemanı: {eleman}");
+        }
+
+        // Diziyi yeniden boyutlandır
+        Array.Resize(ref dizi, dizi.Length + 1);
+
+        // Kullanıcıdan yeni bir eleman al
+        Console.WriteLine("Diziye eklemek istediğiniz sayıyı girin: ");
+        dizi[dizi.Length - 1] = int.Parse(Console.ReadLine());
+
+        // Diziyi sıralama ve ekrana yazdırma
+        Console.WriteLine("Dizinin elemanları (sıralı): ");
+        Array.Sort(dizi); // Küçükten büyüğe sıralar
+        foreach (int eleman in dizi)
+        {
+            Console.WriteLine(eleman);
+           
+        }
+        
+    }
+}
